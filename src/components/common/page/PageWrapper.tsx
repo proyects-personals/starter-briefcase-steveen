74f65666e2ec6baddcb65a1260fee690b312fe61
steveen ordoñez <zh311505@gmail.com>
2024-07-01 10:25:17 -0500

    feat: animated figure

--- src/components/common/page/PageWrapper.tsx
+++ src/components/common/page/PageWrapper.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTheme } from '../../../hook/theme';
+import './styles.css'; // Archivo CSS para definir las animaciones
 
 type PageWrapperProps = {
   children: React.ReactNode;
@@ -8,9 +9,26 @@ type PageWrapperProps = {
 const PageWrapper: React.FC<PageWrapperProps> = ({ children }) => {
   const { isDarkTheme } = useTheme();
 
+  const wrapperStyle = {
+    position: 'relative' as const,
+    minHeight: '100vh',
+    padding: '0 8px',
+    backgroundColor: isDarkTheme ? '#333' : '#f0f0f0',
+    overflow: 'hidden',
+  };
+
   return (
-    <div className={`${isDarkTheme ? 'bg-dark-primary' : 'bg-light-primary'} min-h-screen px-8`}>
-      <main>
+    <div className={`${isDarkTheme ? 'bg-dark-primary' : 'bg-light-primary'}`} style={wrapperStyle}>
+      <div className="circle circle-top-left"></div>
+      <div className="circle circle-top-center"></div>
+      <div className="circle circle-top-right"></div>
+      <div className="circle circle-bottom-left"></div>
+      <div className="circle circle-bottom-center"></div>
+      <div className="circle circle-bottom-right"></div>
+
+      <div className="circle circle-center"></div>
+
+      <main style={{ position: 'relative', zIndex: 1 }}>
         {children}
       </main>
     </div>
--- src/pages/modules/briefcases/Briefcase.tsx
+++ src/pages/modules/briefcases/Briefcase.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import { useLanguage } from "../../../hook/lenguage";
 import { useTheme } from "../../../hook/theme";
 import PageWrapper from "../../../components/common/page/PageWrapper";
+import './styles.css';
 
 const Briefcase: React.FC = () => {
   const { translations } = useLanguage();
   const { isDarkTheme } = useTheme();
+
   return (
     <PageWrapper>
-      <div>
-        <h2
-          className={`${
-            isDarkTheme ? "text-dark-text" : "text-ligth-text"
-          } text-xl font-bold`}
-        >
-          {translations["briefcase.title"]}
-        </h2>
+      <div className="flex flex-col items-center justify-center h-screen text-center">
+        <p className="intro-id mt-4 text-2xl font-bold">Hola, soy Dayana Carrera</p>
+        <p className="intro-id text-2xl font-bold">PERO PUEDES LLAMARME Dayita</p>
+        <p className="intro-id mt-2 text-xl">Soy Tecnologa en Desarrollo de software</p>
+        <div className="mt-6 flex flex-col items-center">
+          <button className="custom-button">
+            → ver mis proyectos
+          </button>
+          <button className="custom-button mt-2">
+            → aprender más
+          </button>
+        </div>
       </div>
     </PageWrapper>
   );
