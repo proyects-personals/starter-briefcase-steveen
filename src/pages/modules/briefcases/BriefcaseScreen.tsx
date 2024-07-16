12137c61f2afc3fed212d7dc62d5745edf1f0a86
steveen ordoñez <zh311505@gmail.com>
2024-07-16 11:33:13 -0500

    feat: presentation

--- src/pages/modules/briefcases/BriefcaseScreen.tsx
+++ src/pages/modules/briefcases/BriefcaseScreen.tsx
@@ -20,15 +20,27 @@ const BriefcaseScreen: React.FC<BriefcaseScreenProps> = () => {
   return (
     <PageWrapperAnimated>
       <div className={`flex flex-col items-center justify-center h-screen text-center ${isDarkTheme ? 'dark-mode' : 'light-mode'}`}>
-        <p className="intro-id mt-4 text-2xl font-bold">{translations['briefcase.hello']}, {translations['briefcase.name']}</p>
-        <p className="intro-id text-2xl font-bold">{translations['briefcase.title']}</p>
-        <p className="intro-id mt-2 text-xl">{translations['briefcase.profession']}</p>
+        <p className={`intro-id mt-4 text-2xl font-bold ${isDarkTheme ? 'text-white' : 'text-black'}`}>
+          {translations['briefcase.hello']}, {translations['briefcase.name']}
+        </p>
+        <p className={`intro-id text-2xl font-bold ${isDarkTheme ? 'text-white' : 'text-black'}`}>
+          {translations['briefcase.title']}
+        </p>
+        <p className={`intro-id mt-2 text-xl ${isDarkTheme ? 'text-white' : 'text-black'}`}>
+          {translations['briefcase.profession']}
+        </p>
         <div className="mt-6 flex flex-col items-center">
-          <button className="custom-button">
+        <button className="custom-button mt-2" onClick={handleNavigateToPresentation}>
+            {translations['briefcase.learn_more']}
+          </button>
+          <button className="custom-button mt-2" onClick={handleNavigateToPresentation}>
+            {translations['briefcase.work_experience']}
+          </button>
+          <button className="custom-button mt-2" onClick={handleNavigateToPresentation}>
             {translations['briefcase.see_projects']}
           </button>
           <button className="custom-button mt-2" onClick={handleNavigateToPresentation}>
-            {translations['briefcase.learn_more']}
+            {translations['briefcase.studies']}
           </button>
         </div>
       </div>
